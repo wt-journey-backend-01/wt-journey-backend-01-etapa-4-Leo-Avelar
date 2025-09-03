cb833053eb167f5ea39bdff551a8d50a3e69b8b2
Leo-Avelar <leonardo.savelar@gmail.com>
2025-09-03 07:15:38 -0300

    retry

--- controllers/agentesController.js
+++ controllers/agentesController.js
@@ -9,7 +9,6 @@ const { z } = require('zod');
 function isValidDate(dateString) {
     const regex = /^\d{4}-\d{2}-\d{2}$/;
     if (!regex.test(dateString)) return false;
-
     const date = new Date(dateString);
     return date instanceof Date && !isNaN(date) && date <= new Date();
 }
